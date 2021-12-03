35b6a5651b35016e9236c3e6fcb906c368c60b2d
JohnyKidd <enyedi.tibor1996@gmail.com>
2021-12-03 11:44:18 +0100

    400 - bad request error on POST method at fetching nutrition information

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-async function getFood() {
-    const response = await fetch("https://trackapi.nutritionix.com/v2/search/instant", {
+async function getFood(searchKey) {
+    const response = await fetch(`https://trackapi.nutritionix.com/v2/search/instant?query=${searchKey}`, {
         method: 'GET',
         headers: {
             'x-app-id': '76b7362a',
@@ -8,6 +8,23 @@ async function getFood() {
         },
     })
     const data = await response.json()
-    console.log(data)
+    return data
 }
-getFood()
+async function getNutrition() {
+    const response = await fetch(`https://trackapi.nutritionix.com/v2/natural/nutrients`, {
+        method: 'POST',
+        headers: {
+            'x-app-id': '76b7362a',
+            'x-app-key': '49d670b5e52cd7fc5cf4cfd48cd5762e',
+            'x-remote-user-id': '0',
+        },
+        body: JSON.stringify({
+            'query':'apple'
+        }),
+    })
+    const data = await response.json()
+    return data
+}
+
+console.log(getFood("apple"))
+console.log(getNutrition())
